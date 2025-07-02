bbf26b4152655482efa9903aae489f724c27a006
vitordrs <soaresvitor196@gmail.com>
2025-07-02 16:23:17 -0300

    comprimindo js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 const paths = {
     scss: {
         src: 'src/*.scss',
         dest: 'dist/'
+    },
+    js:{
+        src:'src/*.js',
+        dest:'dist/'
     }
 };
 
@@ -15,13 +21,24 @@ function compilarSass() {
         .pipe(cleanCSS())
         .pipe(gulp.dest(paths.scss.dest));
 }
+function compressJS(){
+    return gulp.src('src/*.js')
+        .pipe(babel({
+            presets:['@babel/preset-env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+}
 
 function watch() {
     console.log('watching:', paths.scss.src);  // para debug
     gulp.watch(paths.scss.src, compilarSass);
+    gulp.watch(paths.js.src, compressJS);
 }
+
 
 exports.default = gulp.series(
     compilarSass,
+    compressJS,
     watch
 );
